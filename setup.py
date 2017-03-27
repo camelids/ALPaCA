77718b71e30ff6ce0911f86d5042ff9a8d11a20c
Noah Vesely <noah@freedom.press>
2017-03-27 03:46:16 -0700

    Create an ALPaCA package

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hack')
+
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(name='ALPaCA',
+      version='0.2.0',
+      description='A server-side website fingerprinting defense',
+      author='camelids',
+      url='https://github.com/camelids/ALPaCA/tree/strings',
+      packages=['alpaca'],
+      install_requires=reqs
+     )
