6337aa47bae321deaa68b0c74c5069bb23f5669e
Noah Vesely <noah@freedom.press>
2017-03-28 09:11:36 -0700

    file -> __file__ fix in alpaca.dists

--- alpaca/dists/__init__.py
+++ alpaca/dists/__init__.py
@@ -1,6 +1,6 @@
 from os.path import abspath, dirname, join
 
-ABS_FILE_DIR_NAME=abspath(dirname(file))
+ABS_FILE_DIR_NAME=abspath(dirname(__file__))
 
 counts = join(ABS_FILE_DIR_NAME, 'counts.kde')
 html = join(ABS_FILE_DIR_NAME, 'html.kde')
