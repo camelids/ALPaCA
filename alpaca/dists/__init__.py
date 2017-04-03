ff33b7257b1a5b5fe692b5298213f6193481a68a
Noah Vesely <noah@freedom.press>
2017-04-03 14:37:58 -0700

    Don't declare ABS_FILE_DIR_NAME global in alpaca.dists

--- alpaca/dists/__init__.py
+++ alpaca/dists/__init__.py
@@ -1,7 +1,5 @@
 from os.path import abspath, dirname, join
 
-ABS_FILE_DIR_NAME=abspath(dirname(__file__))
-
-counts = join(ABS_FILE_DIR_NAME, 'counts.kde')
-html = join(ABS_FILE_DIR_NAME, 'html.kde')
-objects = join(ABS_FILE_DIR_NAME, 'objects.kde')
+counts = join(abspath(dirname(__file__)), 'counts.kde')
+html = join(abspath(dirname(__file__)), 'html.kde')
+objects = join(abspath(dirname(__file__)), 'objects.kde')
